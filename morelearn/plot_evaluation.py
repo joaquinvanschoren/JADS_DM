e6bf68b1690ac233a9348f806aa8ae0183cb33fb
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2017-03-07 14:48:21 +0100

    first commit

---
+++ morelearn/plot_evaluation.py
@@ -0,0 +1,135 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from .plot_helpers import discrete_scatter
+from .plot_classifiers import plot_classifiers
+from .plot_2d_separator import plot_2d_classification, plot_2d_separator
+from .tools import heatmap
+from .datasets import make_forge
+from sklearn.datasets import make_blobs
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_breast_cancer
+
+def plot_kNN_overfitting_curve(k = range(1, 11)):
+    cancer = load_breast_cancer()
+    X_train, X_test, y_train, y_test = train_test_split(
+        cancer.data, cancer.target, stratify=cancer.target, random_state=66)
+
+    # Build a list of the training and test scores for increasing k
+    training_accuracy = []
+    test_accuracy = []
+
+    for n_neighbors in k:
+        # build the model
+        clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X_train, y_train)
+        # record training and test set accuracy
+        training_accuracy.append(clf.score(X_train, y_train))
+        test_accuracy.append(clf.score(X_test, y_test))
+
+    plt.plot(k, training_accuracy, label="training accuracy")
+    plt.plot(k, test_accuracy, label="test accuracy")
+    plt.ylabel("Accuracy")
+    plt.xlabel("n_neighbors")
+    _ = plt.legend()
+
+def plot_kNN_overfitting(k = [1, 3, 9]):
+    fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+    X, y = make_forge()
+
+    for n_neighbors, ax in zip(k, axes):
+        clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+        plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+        discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+        ax.set_title("{} neighbor(s)".format(n_neighbors))
+        ax.set_xlabel("feature 0")
+        ax.set_ylabel("feature 1")
+    _ = axes[0].legend(loc=3)
+
+def plot_grid_results(c = [0.001, 0.01, 0.1, 1, 10, 100], gamma = [0.001, 0.01, 0.1, 1, 10, 100] ):
+    param_grid = {'C': c,
+              'gamma': gamma}
+    grid_search = GridSearchCV(SVC(), param_grid, cv=5)
+    iris = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(
+        iris.data, iris.target, random_state=0)
+    grid_search.fit(X_train, y_train)
+    results = pd.DataFrame(grid_search.cv_results_)
+    scores = np.array(results.mean_test_score).reshape(6, 6)
+    heatmap(scores, xlabel='gamma', xticklabels=param_grid['gamma'],
+            ylabel='C', yticklabels=param_grid['C'], cmap="viridis");
+
+def plot_holdout_predict_tree():
+    names = ["Decision tree predictions"]
+
+    classifiers = [
+        DecisionTreeClassifier()
+        ]
+
+    plot_classifiers(names, classifiers, figuresize=(20,8))
+
+def plot_holdout(nsamples=100):
+    # create a synthetic dataset
+    X, y = make_blobs(centers=2, random_state=0, n_samples = nsamples)
+    # split data and labels into a training and a test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    discrete_scatter(X_train[:, 0], X_train[:, 1], y_train,
+                             markers='o', ax=ax)
+    discrete_scatter(X_test[:, 0], X_test[:, 1], y_test,
+                             markers='^', ax=ax)
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+    ax.legend(["Train class 0", "Train class 1", "Test class 0",
+                    "Test class 1"], ncol=4,  loc=(-0.1, 1.1));
+
+def plot_overfitting(degrees = [1, 4, 15]):
+    np.random.seed(0)
+
+    n_samples = 30
+
+    true_fun = lambda X: np.cos(1.5 * np.pi * X)
+    X = np.sort(np.random.rand(n_samples))
+    y = true_fun(X) + np.random.randn(n_samples) * 0.1
+
+    plt.figure(figsize=(14, 5))
+    for i in range(len(degrees)):
+        ax = plt.subplot(1, len(degrees), i + 1)
+        plt.setp(ax, xticks=(), yticks=())
+
+        polynomial_features = PolynomialFeatures(degree=degrees[i],
+                                                 include_bias=False)
+        linear_regression = LinearRegression()
+        pipeline = Pipeline([("polynomial_features", polynomial_features),
+                             ("linear_regression", linear_regression)])
+        pipeline.fit(X[:, np.newaxis], y)
+
+        # Evaluate the models using crossvalidation
+        scores = cross_val_score(pipeline, X[:, np.newaxis], y,
+                                 scoring="neg_mean_squared_error", cv=10)
+
+        X_test = np.linspace(0, 1, 100)
+        plt.plot(X_test, pipeline.predict(X_test[:, np.newaxis]), label="Model")
+        plt.plot(X_test, true_fun(X_test), label="True function")
+        plt.scatter(X, y, label="Samples")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.xlim((0, 1))
+        plt.ylim((-2, 2))
+        plt.legend(loc="best")
+        plt.title("Degree {}\nMSE = {:.2e}(+/- {:.2e})".format(
+            degrees[i], -scores.mean(), scores.std()))
+    plt.show()
---
+++ morelearn/plots.py
@@ -0,0 +1,4 @@
+from .plot_helpers import cm2, cm3
+from .plot_evaluation import plot_holdout, plot_overfitting, plot_holdout_predict_tree,plot_grid_results, plot_kNN_overfitting, plot_kNN_overfitting_curve
+
+__all__ = ['plot_holdout','plot_overfitting','plot_holdout_predict_tree','plot_grid_results','plot_kNN_overfitting','plot_kNN_overfitting_curve']
