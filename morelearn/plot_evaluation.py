98a87bca02ada5b43e960d80ba92c7d6b6d38688
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2017-03-16 02:49:09 +0100

    updates on ensemble lecture

--- morelearn/plot_evaluation.py
+++ morelearn/plot_evaluation.py
@@ -10,12 +10,58 @@ from sklearn.datasets import load_digits, load_iris, load_breast_cancer
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split
+from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split, ShuffleSplit
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import precision_recall_curve, roc_auc_score, roc_curve, accuracy_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+
+# Bias-Variance Computation (helper function)
+def compute_bias_variance(clf, X, y):
+    # Bootstraps
+    n_repeat = 100
+    shuffle_split = ShuffleSplit(test_size=0.33, n_splits=n_repeat)
+
+    # Store sample predictions
+    y_all_pred = [[] for _ in range(len(y))]
+
+    # Train classifier on each bootstrap and score predictions
+    for i, (train_index, test_index) in enumerate(shuffle_split.split(X)):
+        # Train and predict
+        clf.fit(X[train_index], y[train_index])
+        y_pred = clf.predict(X[test_index])
+
+        # Store predictions
+        for i,index in enumerate(test_index):
+            y_all_pred[index].append(y_pred[i])
+
+    # Compute bias, variance, error
+    bias_sq = sum([ (1 - x.count(y[i])/len(x))**2 * len(x)/n_repeat
+                for i,x in enumerate(y_all_pred)])
+    var = sum([((1 - ((x.count(0)/len(x))**2 + (x.count(1)/len(x))**2))/2) * len(x)/n_repeat
+               for i,x in enumerate(y_all_pred)])
+
+    return np.sqrt(bias_sq), var
+
+def plot_bias_variance_random_forest():
+    X, y = load_breast_cancer(return_X_y = True)
+
+    bias_scores = []
+    var_scores = []
+    n_estimators= [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+    for i in n_estimators:
+        b,v = compute_bias_variance(RandomForestClassifier(random_state=0,n_estimators=i,n_jobs=-1),X,y)
+        bias_scores.append(b)
+        var_scores.append(v)
+
+    plt.figure(figsize=(5,2))
+    plt.plot(n_estimators, var_scores,label ="variance" )
+    plt.plot(n_estimators, np.square(bias_scores),label ="bias^2")
+    plt.xscale('log',basex=2)
+    plt.xlabel("n_estimators")
+    plt.legend(loc="best")
+    plt.show()
 
 def plot_confusion_matrix():
     digits = load_digits()
