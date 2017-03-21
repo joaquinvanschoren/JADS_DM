dbd809dd89565822f24f4f806d1b85e3ed59b22e
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2017-03-21 14:54:24 +0100

    lecture 10

--- morelearn/plots.py
+++ morelearn/plots.py
@@ -3,6 +3,7 @@ from .plot_evaluation import plot_holdout, plot_overfitting, plot_holdout_predic
 from .plot_evaluation import plot_kNN_regression, plot_bias_variance_random_forest, plot_precision_recall_curve, plot_precision_recall_select, plot_roc, plot_roc_select, plot_roc_imbalanced, plot_confusion_matrix
 from .plot_trees import plot_heuristics, plot_tree, plot_regression_tree, plot_random_forest, plot_tree_extrapolate
 from .plot_classifiers import plot_classifiers
+from .plot_neural import plot_neural_predictions, plot_gradient_descent_surface, plot_gradient_descent
 
 
-__all__ = ['plot_kNN_regression','plot_bias_variance_random_forest','plot_tree_extrapolate','plot_random_forest', 'plot_tree', 'plot_regression_tree', 'plot_classifiers', 'plot_heuristics', 'plot_holdout','plot_overfitting','plot_holdout_predict_tree','plot_grid_results','plot_kNN_overfitting','plot_kNN_overfitting_curve','plot_precision_recall_curve','plot_precision_recall_select','plot_roc','plot_roc_select','plot_roc_imbalanced','plot_confusion_matrix']
+__all__ = ['plot_gradient_descent_surface','plot_gradient_descent','plot_neural_predictions','plot_kNN_regression','plot_bias_variance_random_forest','plot_tree_extrapolate','plot_random_forest', 'plot_tree', 'plot_regression_tree', 'plot_classifiers', 'plot_heuristics', 'plot_holdout','plot_overfitting','plot_holdout_predict_tree','plot_grid_results','plot_kNN_overfitting','plot_kNN_overfitting_curve','plot_precision_recall_curve','plot_precision_recall_select','plot_roc','plot_roc_select','plot_roc_imbalanced','plot_confusion_matrix']
